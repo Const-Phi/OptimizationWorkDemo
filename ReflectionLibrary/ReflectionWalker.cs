ec2836e5713dfcc79cf4a1158dc572ca243331fc
Constantine <konstantin-649@mail.ru>
2017-11-26 02:03:25 +0300

    Add some new DLL (as one more project in solution) which relaesed reflection mechanism to full enumerating (as one long string yet) all of subsenders between base sender and some property (which maybe property of base sender or of one of child subsenders).

---
+++ ReflectionLibrary/ReflectionWalker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace ReflectionLibrary
+{
+    public static class ReflectionWalker
+    {
+        private static String GetFullPath(Object sender, String propertyName, StringBuilder tailBuffer)
+        {
+            if (sender is SenderWrapper wrapper)
+                return GetFullPath(wrapper.InnerSender, propertyName,
+                    tailBuffer.AppendLine(wrapper.OuterSender.GetType().FullName));
+            
+            var senderType = sender.GetType();
+            var newValue = senderType.GetProperty(propertyName)?.GetValue(sender);
+            tailBuffer.AppendLine(senderType.FullName);
+            tailBuffer.AppendLine($"\tproperty \"{propertyName}\" changed with new value {newValue}.");
+            return tailBuffer.ToString();
+        }
+        
+        public static String GetFullPath(Object sender, String propertyName)
+            => GetFullPath(sender, propertyName, new StringBuilder());
+    }
+}
